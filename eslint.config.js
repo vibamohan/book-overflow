fccbaed50e5e501d8327934da824356f49e99d48
Viba Mohan <happymorning552@gmail.com>
2025-12-21 18:41:52 -0800

    Initial commit
    Fix repo structure by resetting it
    
    Signed-off-by: Viba Mohan <happymorning552@gmail.com>
    
    CI First Draft
    
    Signed-off-by: Viba Mohan <happymorning552@gmail.com>
    
    Update CI Versions
    
    Signed-off-by: Viba Mohan <happymorning552@gmail.com>
    
    more minor ci fixes (reminder: squash all commits later)
    
    Signed-off-by: Viba Mohan <happymorning552@gmail.com>
    
    one more
    
    Signed-off-by: Viba Mohan <happymorning552@gmail.com>
    
    Add eas-cli dep
    
    Signed-off-by: Viba Mohan <happymorning552@gmail.com>
    
    Fix lockfile
    
    Simplify CI Workflow
    
    Signed-off-by: Viba Mohan <happymorning552@gmail.com>
    
    App.json includes EAS now (hopefully)
    
    Signed-off-by: Viba Mohan <happymorning552@gmail.com>
    
    Add packages
    
    Signed-off-by: Viba Mohan <happymorning552@gmail.com>
    
    Mv package
    
    Signed-off-by: Viba Mohan <happymorning552@gmail.com>
    
    Forget about app and just use web; add back app later
    
    Signed-off-by: Viba Mohan <happymorning552@gmail.com>
    
    Add in build:web for CI
    
    Signed-off-by: Viba Mohan <happymorning552@gmail.com>
    
    i think i fixed it
    
    Signed-off-by: Viba Mohan <happymorning552@gmail.com>
    
    More changes need to amalmagate
    
    Signed-off-by: Viba Mohan <happymorning552@gmail.com>
    
    restart since it's easier to start from react web then gradually convert to react-native
    
    Signed-off-by: Viba Mohan <happymorning552@gmail.com>
    
    Add vite stuff
    
    Signed-off-by: Viba Mohan <happymorning552@gmail.com>
    
    better state
    
    Signed-off-by: Viba Mohan <happymorning552@gmail.com>

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,23 @@
-// https://docs.expo.dev/guides/using-eslint/
-const { defineConfig } = require('eslint/config');
-const expoConfig = require('eslint-config-expo/flat');
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
+import { defineConfig, globalIgnores } from 'eslint/config'
 
-module.exports = defineConfig([
-  expoConfig,
+export default defineConfig([
+  globalIgnores(['dist']),
   {
-    ignores: ['dist/*'],
+    files: ['**/*.{ts,tsx}'],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      reactHooks.configs.flat.recommended,
+      reactRefresh.configs.vite,
+    ],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
   },
-]);
+])
