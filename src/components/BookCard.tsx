84f589864f5cd3ec73aa592d231c9cb14104d162
Viba Mohan <happymorning552@gmail.com>
2025-12-31 20:16:54 -0800

    Images now render in card
    Signed-off-by: Viba Mohan <happymorning552@gmail.com>

--- src/components/BookCard.tsx
+++ src/components/BookCard.tsx
@@ -3,7 +3,16 @@ import { ClaimButton } from "./ClaimButton";
 
 export function BookCard({ book }: { book: Book }) {
   return (
-    <div className="border rounded p-4">
+    <div className="border rounded p-4 space-y-2">
+      {/* Book image */}
+      {book.image_url && (
+        <img
+          src={book.image_url}
+          alt={book.title}
+          className="w-full h-48 object-cover rounded"
+        />
+      )}
+
       <h2 className="font-semibold">{book.title}</h2>
       <p>{book.subject}</p>
       <p className="text-sm">{book.condition}</p>
